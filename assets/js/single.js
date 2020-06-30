d46c99c24b6dbe47f5a1655fd6c4dbf5c51954a2
alirueter <rueter.alison@gmail.com>
2020-06-30 18:07:44 -0500

    add message if repo has more than 30 open issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,6 @@
 //declare global variables that reference dom elements
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,6 +11,11 @@ var getRepoIssues = function(repo) {
             response.json().then(function(data) {
                 //pass response data to dom function
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
@@ -50,9 +56,21 @@ var displayIssues = function(issues) {
 
         //append to container 
         issueEl.appendChild(typeEl); 
-
         issueContainerEl.appendChild(issueEl);
     }   
 };
 
-getRepoIssues("alirueter/run-buddy");
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    //append to awrning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoIssues("facebook/react");
